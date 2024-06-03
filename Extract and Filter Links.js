37722d119403cbe0359a6cb61695bda1870b3ad1
TPhA <aungaungtamuvillager@gmail.com>
2024-06-03 10:00:30 +0700

    Update Extract and Filter Links.js

--- Extract and Filter Links.js
+++ Extract and Filter Links.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extract and Filter Links
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Extracts and filters links with specified domains and copies them to clipboard as a string
 // @author       You
 // @match        http://www.channelmyanmar.to/*
